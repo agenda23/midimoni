0adf5d7ec317fdee75187703125779c6f641f49d
masataka koguchi <agenda23@gmail.com>
2025-06-07 11:38:33 +0900

    fix errors

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,5 +1,5 @@
 /**
- * By default, Remix will handle hydrating your app on the client for you.
+ * By default, Remix will handle hydrating your app on the client.
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/file-conventions/entry.client
  */
@@ -8,11 +8,22 @@ import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>
-  );
-});
+const hydrate = () => {
+  startTransition(() => {
+    const rootElement = document.getElementById("root") || document;
+    hydrateRoot(
+      rootElement,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>
+    );
+  });
+};
+
+if (typeof requestIdleCallback === "function") {
+  requestIdleCallback(hydrate);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  setTimeout(hydrate, 1);
+}
