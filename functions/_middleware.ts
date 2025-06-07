9531acc2bd7d14f84391f0d5356125e8d17b903b
masataka koguchi <agenda23@gmail.com>
2025-06-07 11:56:33 +0900

    fix errors

--- functions/_middleware.ts
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context: any) {
-  const response = await context.next();
-  
-  // MIDI API用のヘッダーを追加
-  response.headers.set('Permissions-Policy', 'midi=*');
-  response.headers.set('Feature-Policy', 'midi *');
-  
-  return response;
-} 
